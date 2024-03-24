0bfd2dd5f519dfa75d81faa076b968ad1f98dd2d
YEH-YU-YANG <brian5504331@gmail.com>
2024-03-24 23:39:09 +0800

    feat: send_webhook

--- discord.py
+++ discord.py
@@ -23,6 +23,9 @@ import json
 import datetime
 import os
 
+web_hook_url = os.environ['dc_web_hook_url']
+webhook = DiscordWebhook(url=web_hook_url)
+
 # 讀取公告資訊的函式
 def read_announcements_from_json():
     """
@@ -43,3 +46,44 @@ def read_announcements_from_json():
     return announcements
 
 
+def send_webhook():
+    """
+    Reads announcements from JSON, constructs messages, and sends them via Discord webhook.
+
+    Example Usage:
+        # Assuming 'announcements.json' contains announcement data and 'web_hook_url' is set.
+        send_webhook()
+
+    This function reads announcements from a JSON file using read_announcements_from_json(),
+    constructs messages for each announcement, and sends them via Discord webhook.
+    It sends multiple webhook requests if the number of announcements exceeds 10.
+    """
+    announcements = read_announcements_from_json()
+    total_announcements = len(announcements)
+
+    # 計算需要發送的 Webhook 請求次數
+    num_requests = total_announcements // 10 + (1 if total_announcements % 10 != 0 else 0)
+
+    for i in range(num_requests):
+
+        # 計算要處理的公告索引範圍
+        start_index = i * 10
+        end_index = min(start_index + 10, total_announcements)
+        
+        for announcement in announcements[start_index:end_index]:
+
+            
+            # 創建要發送的訊息
+            message = ""
+            message += f"{announcement['time']} - [{announcement['title']}]({announcement['link']})\n"
+
+            # 將訊息添加到 Webhook 中
+            webhook.content = message
+        
+            response = webhook.execute()
+        
+if __name__ == "__main__":
+    try:
+        send_webhook()
+    except Exception as e:
+        print(f"Error: {e}")
